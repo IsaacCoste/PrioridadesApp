9aae9325cb68037a3043cb577d6e1c4829f6dea5
IsaacCoste <isaac24.coste@gmail.com>
2024-10-05 19:14:09 -0400

    Agrege el Modelo de Tickets

---
+++ PrioridadesApp.Data/Models/Tickets.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrioridadesApp.Data.Models;
+public class Tickets
+{
+    [Key]
+    public int TicketId { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public DateTime Fecha { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    [ForeignKey("ClienteId")]
+    public int ClienteId { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    [ForeignKey("SistemaId")]
+    public int SistemaId { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    [ForeignKey("PrioridadId")]
+    public int PrioridadId { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public string? SolicitadoPor { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public string? Asunto { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public string? Descripcion { get; set; }
+}
