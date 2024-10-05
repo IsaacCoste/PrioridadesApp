731697026789f3143b58dcd8130523086a6744d6
IsaacCoste <isaac24.coste@gmail.com>
2024-10-05 19:13:06 -0400

    Agrege el modelo de prioridades

---
+++ PrioridadesApp.Data/Models/Prioridades.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrioridadesApp.Data.Models;
+public class Prioridades
+{
+    [Key]
+    public int PrioridadId { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public string? Descripción { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public int DiasCompromiso { get; set; }
+}
