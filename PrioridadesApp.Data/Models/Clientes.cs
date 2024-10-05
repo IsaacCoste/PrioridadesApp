a56b4652caccdacb9e89829052b644b53429e38e
IsaacCoste <isaac24.coste@gmail.com>
2024-10-05 19:13:32 -0400

    Agrege el modelo de Clientes

---
+++ PrioridadesApp.Data/Models/Clientes.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrioridadesApp.Data.Models;
+public class Clientes
+{
+    [Key]
+    public int ClienteId { get; set; }
+    [StringLength(30, ErrorMessage = "No Puede Exceder los 30 Caracteres")]
+    [Required(ErrorMessage = "El campo Nombre es requerido")]
+    public string? Nombre { get; set; }
+    [StringLength(10, ErrorMessage = "No Puede Exceder los 10 Caracteres")]
+    [Required(ErrorMessage = "El campo Telefono es requerido")]
+    public string? Telefono { get; set; }
+    [StringLength(10, ErrorMessage = "No Puede Exceder los 10 Caracteres")]
+    [Required(ErrorMessage = "El campo Celular es requerido")]
+    public string? Celular { get; set; }
+    [StringLength(11, ErrorMessage = "No Puede Exceder los 11 Caracteres")]
+    [Required(ErrorMessage = "El campo Rnc es requerido")]
+    public string? Rnc { get; set; }
+    [EmailAddress(ErrorMessage = "El email no es valido")]
+    [Required(ErrorMessage = "El campo Email es requerido")]
+    public string? Email { get; set; }
+    [StringLength(60, ErrorMessage = "No Puede Exceder los 60 Caracteres")]
+    [Required(ErrorMessage = "El campo Direccion es requerido")]
+    public string? Direccion { get; set; }
+}
