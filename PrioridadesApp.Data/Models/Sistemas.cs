4145554bbf9430aca4fbda6b77629b5b9f2753e6
IsaacCoste <isaac24.coste@gmail.com>
2024-10-05 19:13:48 -0400

    Agrege el Modelo de Sistemas

---
+++ PrioridadesApp.Data/Models/Sistemas.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrioridadesApp.Data.Models;
+public class Sistemas
+{
+    [Key]
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    public int SistemasId { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio")]
+    [RegularExpression(@"^[A-Za-z0-9\s]*$", ErrorMessage = "El nombre del sistema solo puede contener letras, números y espacios")]
+    public string? SistemaNombre { get; set; }
+}
